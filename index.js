87c4409962be3c723cacda38df592aec85d6e039
Scott Keplinger <skeplin17@gmail.com>
2020-01-14 22:37:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ function dec(n) {
 }
 
 function makeInt(n) {
-  return parseInt ('n', 10)
+  return parseInt (n, 10)
 }
 
 function preserveDecimal(n) {
- return parseFloat('n')
+ return parseFloat(n)
 }
